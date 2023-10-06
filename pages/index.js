102901993e5cfce039723cd67ccca38afe2102f0
Antony Maziero Warken <againplayi7@gmail.com>
2023-10-06 12:45:44 -0300

    cap 3 vide 1 finalizado

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
-import Link from '../src/components/Link';
+import Link from "../src/components/Link";
 
-
+function Title({ children, as }) {
+    const Tag = as
+  return (
+    <>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+        }
+      `}</style>
+    </>
+  );
+}
 export default function HomePage() {
-    return (
-        <div>
-            <h1>Alura Cases - Home</h1>
-            <Link href="/faq">
-                Ir para a página do FAQ
-            </Link>
-        </div>
-    )
+  return (
+    <div>
+      <Title as='h1'>Alura Cases - Home</Title>
+      <Link href="/faq">Ir para a página do FAQ</Link>
+    </div>
+  );
 }
